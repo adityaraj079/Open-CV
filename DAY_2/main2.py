833eef8c8173d7997cb425755634b2d564f71d0b
adityaraj079 <55791929+adityaraj079@users.noreply.github.com>
2021-01-03 13:51:33 +0530

    Add files via upload

---
+++ DAY_2/main2.py
@@ -0,0 +1,16 @@
+import cv2 
+import numpy as np
+
+img = np.zeros((512,512,3),np.uint8)
+#print(img)
+#img[:] = 255,0,0
+
+cv2.line(img,(0,0),(300,300),(0,255,0),3)
+cv2.rectangle(img,(0,0),(250,250),(0,0,255))
+cv2.rectangle(img,(250,250),(300,300),(0,0,255),cv2.FILLED)
+cv2.circle(img,(400,50),30,(255,255,0),5)
+cv2.putText(img," OPEN-CV ",(300,200),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),1)
+
+cv2.imshow("Image",img)
+
+cv2.waitKey(0)
